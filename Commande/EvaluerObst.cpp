01a77b9e09a5ea74929e18b6c7aa26f4ccb9221a
Mael AUDREN DE KERDREL <audrenmael@gmail.com>
2015-01-05 19:14:58 +0100

    ajout de EvaluerObst

--- Commande/EvaluerObst.cpp
+++ Commande/EvaluerObst.cpp
@@ -7,6 +7,7 @@
 //
 
 #include "EvaluerObst.h"
+#include "iostream"
 
 Commande* EvaluerObst::constructeurVirtuel(LireEntree* e){
     return new EvaluerObst();
@@ -14,9 +15,9 @@ Commande* EvaluerObst::constructeurVirtuel(LireEntree* e){
 
 void EvaluerObst::execute(){
     Commande::commandeUtilisees.push(this);
-    robot->evaluerPlot();
+    std::cout<<"Le plot a une taille de "<<robot->evaluerPlot()<<std::endl;
 }
 
 void EvaluerObst::undo(){
-	  Commande::commandeUtilisees.pop();
+  Commande::commandeUtilisees.pop();
 }
